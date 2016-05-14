985729da28c5b864267d8a4a7a5ba28c947437e0
chenzhengxin <chen_zhengxin@hotmail.com>
2016-05-14 10:46:46 +0800

    删除obj文件夹，修复计量单位切换时各参数显示问题

--- OBJ/ff.d
+++
@@ -1,6 +0,0 @@
-..\OBJ\ff.o: ..\FATFS\src\ff.c
-..\OBJ\ff.o: ..\FATFS\src\ff.h
-..\OBJ\ff.o: ..\FATFS\src\integer.h
-..\OBJ\ff.o: ..\FATFS\src\ffconf.h
-..\OBJ\ff.o: ..\FATFS\src\diskio.h
-..\OBJ\ff.o: d:\Keil\ARM\RV31\Inc\stdarg.h
